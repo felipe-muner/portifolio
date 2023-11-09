5fe4efe06bfd5aa9b7555ba2f2a326c1123562f7
Felipe Muner <felipe.muner@gmail.com>
2023-11-09 12:14:20 +0700

    feat: multi theme (atm dark and light)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import ThemeButton from "@/components/theme-button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body className={`${inter.className} min-h-screen`}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {/* Header */}
+          <header className="py-6">
+            <nav className="container flex items-center justify-between">
+              <div className=" dark:bg-red-600 bg-blue-800 dark:hover:bg-zinc-700">
+                proof theme switcher
+              </div>
+              <ThemeButton />
+            </nav>
+          </header>
+
+          {/* Page */}
+          <main>{children}</main>
+
+          {/* Footer */}
+          <footer></footer>
+        </Providers>
       </body>
     </html>
   );
