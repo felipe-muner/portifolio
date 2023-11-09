e02e8ad059071110087320d978b14ca56b07bad4
felipe muner <felipe.muner@gmail.com>
2023-11-09 12:16:07 +0700

    feat: multi theme (atm dark and light) (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   //@TODO: implement to have line 20 and 21 available
   // const { page, auth } = await getDictionary(lang)
   const session = await getServerSession(authOptions);
-  
+
   const user = session?.user;
 
   return (
@@ -23,7 +23,9 @@ export default async function Home() {
           <pre className="mt-4">
             <code>{JSON.stringify({ name: user?.name }, null, 2)}</code>
           </pre>
-
+          <span className="dark:hover:bg-green-300 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
           <AuthButton />
         </div>
       </div>
