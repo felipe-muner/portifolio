3794ae26ec2bb7923928af998d4aded9fceb4aeb
felipe muner <felipe.muner@gmail.com>
2023-11-12 22:19:36 +0700

    add header to layout (#21)

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -17,20 +17,19 @@ const ThemeButton = () => {
 
   return (
     <>
-    resolvedTheme: {resolvedTheme}
-    <button
-      aria-label="Toggle Dark Mode"
-      type="button"
-      className="flex items-center justify-center rounded-lg p-2 transition-colors hover:bg-zinc-100 dark:hover:bg-zinc-700"
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        className="flex items-center justify-center rounded-lg p-2 transition-colors hover:bg-zinc-100 dark:hover:bg-zinc-700"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
       >
-      {resolvedTheme === "dark" ? (
-        <SunIcon className="h-5 w-5 text-orange-300" />
+        {resolvedTheme === "dark" ? (
+          <SunIcon className="h-5 w-5 text-orange-300" />
         ) : (
           <MoonIcon className="h-5 w-5 text-slate-800" />
-          )}
-    </button>
-          </>
+        )}
+      </button>
+    </>
   );
 };
 
