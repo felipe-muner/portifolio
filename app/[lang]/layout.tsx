110147cf62d6ccf1b6f319b6832f4e5a56830fae
Felipe Muner <felipe.muner@gmail.com>
2023-11-13 09:43:40 +0700

    fix: casing of header and footer component imports.

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { Locale, i18n } from "@/i18n.config";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
